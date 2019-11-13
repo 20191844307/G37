c3c06c0c806d1a205101b36dd6a9dd24da493f8f
unknown <1522296191@qq.com>
2019-11-13 23:20:36 +0800

    第四章第十一题

---
+++ C4/Yan 4.11/4011/StdAfx.h
@@ -0,0 +1,22 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__596F875C_8979_4451_AFB7_F6B4D375903E__INCLUDED_)
+#define AFX_STDAFX_H__596F875C_8979_4451_AFB7_F6B4D375903E__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+
+#include <stdio.h>
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__596F875C_8979_4451_AFB7_F6B4D375903E__INCLUDED_)
