1951a98cd72deff30c878c460d4943a2aa24868f
unknown <13181140681@163.com>
2019-12-04 21:56:35 +0800

    Signed-off-by: unknown <”13181140681@163.com>

---
+++ c7/7.12/c712.cpp
@@ -0,0 +1,30 @@
+// c712.cpp : Defines the entry point for the console application.
+//
+
+#include<stdio.h>
+int main()
+	{
+	 void math();
+	 int a,b,c,d;
+	 int x;
+	 printf("a=\n");
+	 printf("b=\n");
+	 printf("c=\n");
+	 printf("d=\n");
+	 scanf("%d,%d,%d,%d",&a,&b,&c,&d);
+	 printf("%d*x*x*x+%d*x*x+%d*x+%d=0\n");
+	 math(x);
+	 printf("Êä³ö:");
+	 return 0;
+	}
+
+
+void math()
+	{
+	 int x1,x2;
+	 if(fabs(1-x1)<fabs(1-x2))
+		 x=x1;
+	 else x=x2;
+	}
+
+
