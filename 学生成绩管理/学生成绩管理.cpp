d9e43fd076af5d42052998597eeb225a35a75bf1
unknown <13181140681@163.com>
2019-12-17 19:25:53 +0800

    c

--- 学生成绩管理/学生成绩管理.cpp
+++ 学生成绩管理/学生成绩管理.cpp
@@ -54,24 +54,23 @@ int main()
 		  switch(ch)
 			{
 				case'1':Input();
-						break;
+					break;
 				case'2':Print();
-						break;
+					break;
 				case'3':Save();
-						break;
+					break;
 				case'4':Read();
-						break;
+					break;
 				case'5':printf("人数:%d\n",count);
-						break;
+					break;
 				case'6':ave();
-						break;
+					break;
 				case'7':
 					{
 					 Node*pNode=Find();
 					 if(pNode!=NULL)
 						{
-						 printf("学号:%d\t姓名:%s\t年龄:%d\t成绩:%d\n",pNode->stu.num,
-							     pNode->stu.name,pNode->stu.age,pNode->stu.scorea);
+						 printf("学号:%d\t姓名:%s\t年龄:%d\t成绩:%d\n",pNode->stu.num,pNode->stu.name,pNode->stu.age,pNode->stu.scorea);
 						}
 					 break;
 					}
@@ -108,8 +107,7 @@ void Input()
 		{
 		 p->pNext=pNewNode;
 		}
-	 scanf("%s %d %d %d",pNewNode->stu.name,&pNewNode->stu.num,
-		    &pNewNode->stu.age,&pNewNode->stu.scorea);
+	 scanf("%s %d %d %d",pNewNode->stu.name,&pNewNode->stu.num，&pNewNode->stu.age,&pNewNode->stu.scorea);
 	 system("cls");
 	 printf("\n");
 	}
@@ -165,7 +163,7 @@ void Read()
 		 p2=p;
 		}
 	 g_pHead=NULL;
-	 FILE*pFile=fopen("C:\\stuinfo.data","r");
+	 FILE*pFile=fopen("C:\\学生信息.txt","r");
 	 if(pFile==0)
 		{
 		 printf("\n");
